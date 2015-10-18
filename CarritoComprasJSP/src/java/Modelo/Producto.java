a6efef48748d5c32f7468f3976f756b375e3f243
Ax-Angel <angel_r_m2001@hotmail.com>
2015-10-18 14:25:55 -0500

    constructor

--- CarritoComprasJSP/src/java/Modelo/Producto.java
+++ CarritoComprasJSP/src/java/Modelo/Producto.java
@@ -10,5 +10,16 @@ package Modelo;
  * @author -Angel
  */
 public class Producto {
-    
+     private int codigoProducto;
+    private String nombre;
+    private double precio;
+    //Constructor de la clase sin parametros
+    public Producto() {
+    }
+    //Constructor de la clase con parametros
+    public Producto(int codigoProducto, String nombre, double precio) {
+        this.codigoProducto = codigoProducto;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 }
