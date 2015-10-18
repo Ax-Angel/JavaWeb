446167d692f5a61e843d8083a6c1516d979792f2
Ax-Angel <angel_r_m2001@hotmail.com>
2015-10-18 14:26:43 -0500

    getters y setters

--- CarritoComprasJSP/src/java/Modelo/Producto.java
+++ CarritoComprasJSP/src/java/Modelo/Producto.java
@@ -22,4 +22,35 @@ public class Producto {
         this.nombre = nombre;
         this.precio = precio;
     }
+     //Metodo toString de la clase que nos retorna
+    //el nombre del producto
+    @Override
+    public String toString() {
+        return nombre.toUpperCase();
+    }
+    //Metodos get y set de la clase
+    public int getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(int codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
 }
