f7858ca1e4c083f8959c396058966dee6e41fc9e
Ax-Angel <angel_r_m2001@hotmail.com>
2015-10-18 17:28:42 -0500

    adsa

---
+++ CarritoComprasJSP/src/java/servlets/AgregarProducto.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Cliente;
+import modelo.Producto;
+
+/**
+ *
+ * @author Seba
+ */
+@WebServlet(name = "AgregarProducto", urlPatterns = {"/AgregarProducto"})
+public class AgregarProducto extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Agregando...</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            String nombre = request.getParameter("nombre");
+            int id = Integer.valueOf(request.getParameter("id"));
+            String tipo = request.getParameter("tipo");
+            int precio = Integer.valueOf(request.getParameter("precio"));
+            Producto producto = new Producto();
+            producto.setId(id);
+            producto.setNombre(nombre);
+            producto.setTipo(tipo);
+            producto.setPrecio(precio);
+                    
+            Cliente.productos.add(producto);
+            
+            response.sendRedirect("index.jsp");
+            
+        }catch(Exception e){
+                out.print("<script type=text/javascript>");
+                out.print("alert('NO SE PUDO AGREGAR EL PRODUCTO A SU LISTA');");
+                out.print("location.href='index.jsp';");
+                out.print("</script>");
+        }finally {
+            out.println("</body>");
+            out.println("</html>");
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
