e345ad5b6dc468311b359c45e3ca27491dbfd648
Ax-Angel <angel_r_m2001@hotmail.com>
2015-10-18 17:03:39 -0500

    agregue un nuevo metodo de conexion

--- CarritoComprasJSP/src/java/datos/Conexion.java
+++ CarritoComprasJSP/src/java/datos/Conexion.java
@@ -43,4 +43,18 @@ public class Conexion {
             productos.add(producto);
         }return productos;
     }
+    
+     public ArrayList<Producto> buscarProductos(String nombre) throws Exception{
+        conexion();
+        ArrayList<Producto> productos = new ArrayList<Producto>();
+        ResultSet result = state.executeQuery("SELECT * FROM producto WHERE nombre = '"+nombre+"'");
+        while(result.next()){
+            Producto producto = new Producto();
+            producto.setId((Integer) result.getObject(1));
+            producto.setNombre((String) result.getObject(2));
+            producto.setTipo((String) result.getObject(3));
+            producto.setPrecio((Integer) result.getObject(4));
+            productos.add(producto);
+        }return productos;
+    }
 }
