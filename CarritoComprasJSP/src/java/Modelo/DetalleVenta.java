31621113f5cfe1baf7719b8bbb4f7a6dc75bcd88
Ax-Angel <angel_r_m2001@hotmail.com>
2015-10-18 14:32:09 -0500

    setters y getters de detalle venta

--- CarritoComprasJSP/src/java/Modelo/DetalleVenta.java
+++ CarritoComprasJSP/src/java/Modelo/DetalleVenta.java
@@ -28,4 +28,52 @@ public class DetalleVenta {
         this.cantidad = cantidad;
         this.descuento = descuento;
     }
+    
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(int codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public int getCodigoVenta() {
+        return codigoVenta;
+    }
+
+    public void setCodigoVenta(int codigoVenta) {
+        this.codigoVenta = codigoVenta;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Venta getVenta() {
+        return venta;
+    }
+
+    public void setVenta(Venta venta) {
+        this.venta = venta;
+    }
 }
