69f81e750292c0d32c231d06c24b94e93977c63b
Ax-Angel <angel_r_m2001@hotmail.com>
2015-10-18 14:29:06 -0500

    constructores de venta

--- CarritoComprasJSP/src/java/Modelo/Venta.java
+++ CarritoComprasJSP/src/java/Modelo/Venta.java
@@ -10,5 +10,18 @@ package Modelo;
  * @author -Angel
  */
 public class Venta {
-    
+     private int codigoVenta;
+    private String cliente;
+    private Timestamp fecha;
+    //Constructor de la clase sin parametros
+
+    public Venta() {
+    }
+    //Constructor de la clase con parametros
+
+    public Venta(int codigoVenta, String cliente, Timestamp fecha) {
+        this.codigoVenta = codigoVenta;
+        this.cliente = cliente;
+        this.fecha = fecha;
+    }
 }
